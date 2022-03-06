9798ec5d9f8897a9f8adda26c00a842ce2009eb0
HishamAfifi <hish.abdelshafouk@gmail.com>
2022-03-06 07:26:17 -1000

    ...

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ children, ...rest }) => {
     <Route
       {...rest}
       render={() => {
-        return true ? children : <Redirect to="/login" />;
+        return isUser ? children : <Redirect to="/login" />;
       }}
     ></Route>
   );
